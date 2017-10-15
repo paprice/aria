a87af0d220231a67d1e4d969680c61602eaa0fec
Patrice Desrochers <despat-96@outlook.com>
2017-10-14 22:32:10 -0400

    dependance

--- nerc-fr/core/src/main/java/org/vicomtech/cmdline/namefind/EvalToolParams.java
+++
@@ -1,7 +0,0 @@
-package org.vicomtech.cmdline.namefind;
-
-import opennlp.tools.cmdline.params.DetailedFMeasureEvaluatorParams;
-import opennlp.tools.cmdline.params.EvaluatorParams;
-
-interface EvalToolParams extends EvaluatorParams, DetailedFMeasureEvaluatorParams {
-}
