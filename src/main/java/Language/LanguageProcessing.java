1eb802f4914fc63e1207f1fdb303ebaf2cd912e7
Patrice Desrochers <despat-96@outlook.com>
2017-10-16 15:15:25 -0400

    modification tokenizer pour meilleur compréhension par le POS

--- src/main/java/Language/LanguageProcessing.java
+++ src/main/java/Language/LanguageProcessing.java
@@ -6,13 +6,18 @@
 package Language;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import opennlp.tools.cmdline.PerformanceMonitor;
 import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSSample;
 import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.tokenize.WhitespaceTokenizer;
 import opennlp.tools.util.*;
 
@@ -25,50 +30,43 @@ import opennlp.tools.util.PlainTextByLineStream;
 public class LanguageProcessing {
 
     /**
-     * 
+     *
      * @param sentence the sentence send by the user
      * @return the sentence with all the type of the word
-     * @throws IOException 
-     * 
+     * @throws IOException
+     *
      */
     public static String Parse(String sentence) throws IOException {
 
         /*//////////////////////////////////////////////////////////////*
         Syntaxe de retour de la fonction :
         mot_typeDeMotGenreNombre  -> pommes_NCmp (Nom Commun Masculin Pluriels
-        
         NC -> nom commun
         DET -> Déterminant
         CLS -> Pronom
         V/VINF -> Verbe
         ADJ -> Adjectif
-        
         P -> Préposition
         ADV -> Adverbe
         CC -> Conjonction
-        
-        
         syntaxe complète des models : http://www.llf.cnrs.fr/Gens/Abeille/French-Treebank-fr.php
-        
         ////////////////////////////////////////////////////////////*/
-        
-        
         POSModel model = new POSModelLoader()
                 .load(new File("models/fr-pos.bin"));
         PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
         POSTaggerME tagger = new POSTaggerME(model);
-
         ObjectStream<String> lineStream = new PlainTextByLineStream(new StringReader(sentence));
-
         perfMon.start();
         String line;
         String ret = new String();
         while ((line = lineStream.read()) != null) {
-            String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE
-                    .tokenize(sentence);
-            String[] tags = tagger.tag(whitespaceTokenizerLine);
-
-            POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
+            InputStream modelIn = new FileInputStream("models/fr-token.bin");
+            TokenizerModel TokModel;
+            TokModel = new TokenizerModel(modelIn);
+            TokenizerME tokenizer = new TokenizerME(TokModel);
+            String tokens[] = tokenizer.tokenize(sentence);
+            String[] tags = tagger.tag(tokens);
+            POSSample sample = new POSSample(tokens, tags);
             //System.out.println(sample.toString());
             ret = sample.toString();
             perfMon.incrementCounter();
