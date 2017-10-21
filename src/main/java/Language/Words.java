4065374591cbfa08c5839c6234092d5d4df41241
Patrice Desrochers <despat-96@outlook.com>
2017-10-20 22:23:05 -0400

    debut extraction mot important

--- src/main/java/Language/Words.java
+++ src/main/java/Language/Words.java
@@ -8,6 +8,7 @@ package Language;
 import java.lang.reflect.Array;
 import opennlp.tools.postag.POSSample;
 import java.lang.String;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,14 +16,20 @@ import java.lang.String;
  */
 public class Words {
 
-    public static String[] ExtractImportant(POSSample userInput) {
-        
+    public static ArrayList<String> ExtractImportant(POSSample userInput) {
 
         String[] words = userInput.getSentence();
         String[] wordTags = userInput.getTags();
 
-        
+        ArrayList<String> important = new ArrayList<String>();
 
+        for (int i = 0; i < wordTags.length; i++) {
+            if (wordTags[i].equals("NPP") || wordTags[i].equals("NC")) {
+                important.add(words[i]);
+            }
+        }
+
+        return important;
     }
 
 }
--- src/main/java/windows/WindowsController.java
+++ src/main/java/windows/WindowsController.java
@@ -8,6 +8,7 @@ package windows;
 import static Language.LanguageProcessing.Parse;
 import static Language.Words.ExtractImportant;
 import java.io.IOException;
+import java.util.ArrayList;
 import opennlp.tools.postag.POSSample;
 
 /**
@@ -15,14 +16,18 @@ import opennlp.tools.postag.POSSample;
  * @author despa
  */
 public class WindowsController {
- 
-    public static String AiDecortication( String userInput) throws IOException{
+
+    public static String AiDecortication(String userInput) throws IOException {
 
         POSSample parsed = Parse(userInput);
-        String[] important = ExtractImportant(parsed);
+        ArrayList<String> important = ExtractImportant(parsed);
+
+        String ret = "";
+        for(String i : important){
+            ret += i;
+        }
         
-        return parsed.toString();
+        return ret;
     }
-    
-    
+
 }
