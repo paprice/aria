719afc911b7e8ac2c7d9ecc7e197a625c90262a0
Gildo <Gildo@Gildo-PC>
2017-10-29 12:42:13 -0400

    Removed parsing of nouns (Lemmatize renders it useless)

--- src/main/java/InputProcessing/WordParser.java
+++ src/main/java/InputProcessing/WordParser.java
@@ -29,20 +29,28 @@ public class WordParser {
         String[] wordTags = userInput.getTags();
         String[] lemmatize = LemmatizeWord(words);
 
-        List<Document> wordList = new ArrayList<Document>();
+        List<Document> wordList = new ArrayList<>();
 
         for (int i = 0; i < wordTags.length; i++) {
 
-            if (wordTags[i].equals("NC")) {
-                wordList.add(new Document("type", "nc").append("word", lemmatize[i]));
-            } else if (wordTags[i].equals("V")) {
-                wordList.add(new Document("type", "v").append("word", lemmatize[i]));
-            } else if (wordTags[i].equals("ADJ")) {
-                wordList.add(new Document("type", "adj").append("word", lemmatize[i]));
-            } else if (wordTags[i].equals("ADV")) {
-                wordList.add(new Document("type", "adv").append("word", lemmatize[i]));
-            } else if (wordTags[i].equals("NPP")) {
-                wordList.add(new Document("type", "npp").append("word", lemmatize[i]));
+            switch (wordTags[i]) {
+                case "NC":
+                    wordList.add(new Document("type", "nc").append("word", lemmatize[i]));
+                    break;
+                case "V":
+                    wordList.add(new Document("type", "v").append("word", lemmatize[i]));
+                    break;
+                case "ADJ":
+                    wordList.add(new Document("type", "adj").append("word", lemmatize[i]));
+                    break;
+                case "ADV":
+                    wordList.add(new Document("type", "adv").append("word", lemmatize[i]));
+                    break;
+                case "NPP":
+                    wordList.add(new Document("type", "npp").append("word", lemmatize[i]));
+                    break;
+                default:
+                    break;
             }
         }
         
@@ -53,67 +61,6 @@ public class WordParser {
         
         
         return wordList;
-    }
-
-    private static String parseNoun(String noun) {
-        int last = noun.length() - 1;
-        int sndLast = noun.length() - 2;
-
-        //Case singular
-        switch (noun.charAt(last)) {
-
-            //Case plural 's'
-            case 's':
-                //skip if invariable "is"
-                if (noun.endsWith("ois") || noun.endsWith("vis") || noun.endsWith("llis") || noun.endsWith("dis") || noun.endsWith("pis")
-                        || noun.endsWith("quis") || noun.endsWith("uis") || noun.endsWith("dais") || noun.endsWith("rais") || noun.endsWith("chis")
-                        || noun.endsWith("outis") || noun.endsWith("ssis") || noun.endsWith("bis") || noun.endsWith("stis") || noun.endsWith("mmis")
-                        || noun.endsWith("lais") || noun.endsWith("otis")) {
-                    return noun;
-                } //skip if invariable exception
-                else if (noun.length() == 3 || noun.equals("souris") || noun.equals("mépris") || noun.equals("indécis") || noun.equals("vernis")) {
-                    return noun;
-                } //default
-                else {
-                    return noun.substring(0, last);
-                }
-
-            //Case plural 'x'
-            case 'x':
-                //skip if singular form "aux"
-                if (noun.endsWith("aux") && noun.length() == 4) {
-                    return noun;
-                } //if exception form "yeux"
-                else if (noun.equals("yeux")) {
-                    return "oeil";
-                } //if exception form "ail" to "aux"
-                else if (noun.equals("coraux") || noun.equals("fermaux") || noun.equals("gemmaux") || noun.equals("soupiraux")
-                        || noun.equals("vantaux") || noun.equals("ventaux") || noun.equals("vitraux")) {
-                    return noun.substring(0, sndLast) + "il";
-                } //if "oux" form
-                else if (noun.endsWith("oux") && noun.length() > 4) {
-                    return noun.substring(0, last);
-                } //if "eux" form
-                else if (noun.endsWith("eux")) {
-                    return noun.substring(0, last);
-                } //if "eaux" form
-                else if (noun.endsWith("eaux")) {
-                    return noun.substring(0, last);
-                } //if "au" to "aux" form
-                else if (noun.endsWith("yaux") || noun.endsWith("baux") || noun.endsWith("iaux")) {
-                    return noun.substring(0, last);
-                } //if "al" to "aux" form
-                else if (noun.endsWith("aux")) {
-                    return noun.substring(0, sndLast) + "l";
-                } //default skip
-                else {
-                    return noun;
-                }
-
-            //Case singular
-            default:
-                return noun;
-        }
     }
 
     private static String[] LemmatizeWord(String[] word) throws IOException {
