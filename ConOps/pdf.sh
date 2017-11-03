f87eb2f83619c5ca490faa43967c1762c12bb68e
Patrice Desrochers <despat-96@outlook.com>
2017-11-03 16:01:37 -0400

    remove dossier conops

--- ConOps/pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-prefixe=${1%.*}
-commande="pandoc "$1" -f markdown -o "$prefixe".pdf -N --toc -V lof -V lang=fr -V documentclass=scrreprt -V papersize=letter  -H header.txt"
-$commande
